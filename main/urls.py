dc465f21020ad0c6ab9e1512dc12d270c49de6e5
Sandeep Somu <sandeepsomu999@gmail.com>
2023-05-03 01:07:53 -0700

    added save and add new item in the web page

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("v1/",views.v1, name="v1"),
+    path("<int:id>", views.index, name="index"),
+    path("", views.home, name="home"),
+    path("create/", views.create, name = "create"),
 ]
 #start
